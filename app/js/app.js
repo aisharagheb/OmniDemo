cbfaf150a3e52ada648ed3461c4a4b35e02bf999
Jen Rasmussen <jrasmussen@four51.com>
2015-10-22 21:46:41 -0500

    adding modules [ addToCartPreview, securityModal, new termsModal ] / commenting out compile-module for now ( causing an error on minification )

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,6 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'angular-carousel', 'OrderCloud-ProductListAddToCart', 'OrderCloud-ProductLightbox', 'compile-module', 'OrderCloud-Minicart',
-'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse']);
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AddToCartPreview', 'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse', 'OrderCloud-Minicart', 'OrderCloud-ProductLightbox',
+    /*'compile-module', */'OrderCloud-ProductListAddToCart', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
+]);
