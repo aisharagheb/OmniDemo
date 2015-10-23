ac1822a366abafb76eff437382ad66d22572afbc
Jen Rasmussen <jrasmussen@four51.com>
2015-10-23 07:56:34 -0500

    adding largeAddressListSearch

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,6 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
-    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AddToCartPreview', 'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse', 'OrderCloud-Minicart', 'OrderCloud-ProductLightbox',
-    /*'compile-module', */'OrderCloud-ProductListAddToCart', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
+    'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AddToCartPreview', 'OrderCloud-Carousel', 'OrderCloud-CategoryCollapse', 'OrderCloud-LargeAddressListSearch',
+    'OrderCloud-Minicart', 'OrderCloud-ProductLightbox', /*'compile-module', */'OrderCloud-ProductListAddToCart', 'OrderCloud-SecurityModal', 'OrderCloud-TermsModal'
 ]);
